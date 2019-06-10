5d2ed6795a821410276a8bbad950a927a4f3dc2f
gjcode <golla.programmazione@gmail.com>
2019-06-10 12:52:35 +0200

    risolto problema token, da risolvere binding group info

--- broker/api/routes/user.js
+++ broker/api/routes/user.js
@@ -8,16 +8,24 @@ const awsURL = "http://ec2-34-220-162-82.us-west-2.compute.amazonaws.com:5002";
  * */
 
 router.get('/:groupID', (req, res, next) => {
+    console.log(req.header("authorization").split(" ")[1]);
     const groupID = req.params.groupID;
-    console.log(req.headers);
     const clientOptions = {
         uri: awsURL + "/user/" + groupID,
         body: JSON.stringify(req.body),
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'Authorization': 'JWT ' + req.header("authorization").split(" ")[1]
         }
     };
+    request(clientOptions, (error, response) => {
+        if (!error) {
+            return res.status(200).send(response.body);
+        } else {
+            console.error(error);
+        }
+    })
 });
 
 module.exports = router;
