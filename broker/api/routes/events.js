29690dc2f34ca040bb3c8e6363e3d0123de45862
gjcode <golla.programmazione@gmail.com>
2019-06-29 18:03:18 +0200

    risolto problema aggiornamento visivo eventi dopo eliminazione, steso scheletro attivazione camera (non ancora testato)

--- broker/api/routes/events.js
+++ broker/api/routes/events.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const fs = require('fs');
+const mqttClient = new MqttHandler('eventsHandler');
 
 /**
  * permette di gestire lo stato delle console per le varie stanze
@@ -14,7 +15,7 @@ router.post('/status', (req, res, next) => {
     consoleStatus.push(req.body);
     fs.writeFileSync(__dirname + '/../db/consoleStatus.json', JSON.stringify(consoleStatus));
     if (consoleStatus.active) {
-        schedule(req.body.roomId);
+        handleOn(consoleStatus);
     }
     return res.status(200).send(req.body);
 });
@@ -75,6 +76,73 @@ const schedule = function (roomId) {
     let simpleEvents = JSON.parse(fs.readFileSync(__dirname + '/../db/simpEvents.json'));
     let repeatable = repeatableEvents.findIndex(r => r.roomName === roomId) !== -1 ? repeatableEvents.findIndex(r => r.roomName === roomId) : simpleEvents.findIndex(r => r.roomName === roomId);
     repeatable ? scheduleRepeatable(repeatableEvents.find(r => r.roomName === roomId)) : scheduleSimple(simpleEvents.find(r => r.roomName === roomId))
+};
+
+/**
+ * primo processo lanciato quando si riceve un console ON
+ * */
+const handleOn = function (consoleStatus) {
+    const roomId = consoleStatus.roomId;
+    if (consoleStatus.mode === 'programmed') {
+        checkAndActivateProgrammed(roomId);
+    }
+};
+
+const checkAndActivateProgrammed = function (roomId) {
+    let repeatableEvents = JSON.parse(fs.readFileSync(__dirname + '/../db/reapEvents.json'));
+    let index = repeatableEvents.findIndex(ev => ev.roomName === roomId);
+    if (index !== -1) {
+        handleRepeatable(event);
+    }
+};
+
+const handleRepeatable = function (event) {
+    let start = mapDay(event.from.toLowerCase());
+    let end = mapDay(event.to.toLowerCase());
+    let now = new Date();
+    let nowDay = mapDay(now.getDay());
+    if (nowDay <= end && nowDay >= start) {
+        // ora di accendere?
+        let nowTime = now.getHours();
+        let progTemp = event.temp;
+        let startTime = event.startTime;
+        let endTime = event.endTime;
+        if (nowTime >= startTime && nowTime <= endTime) {
+            // supponiamo un grado per ora
+            let diffHours = startTime - nowTime;
+            let room = JSON.parse(fs.readFileSync(__dirname + '/../db/apartment.json')).rooms.find(r => r.id === event.roomName);
+            let sensorId = room.sensor.id;
+            let lastReading = JSON.parse(fs.readFileSync(__dirname + '/../db/last-readings.json')).find(re => re.id === sensorId);
+            // valuto riscaldamento
+            if (progTemp > lastReading) {
+                let diffTemp = progTemp - lastReading;
+                if (diffTemp >= diffHours) {
+                    // riscaldo
+                    mqttClient.sendMessage('command-ha:' + room.heatAct.id, 'on');
+                }
+            } else {
+                let diffTemp = Math.abs(progTemp - lastReading);
+                if (diffTemp >= diffHours) {
+                    // raffreddo
+                    mqttClient.sendMessage('command-ca:' + room.coolAct.id, 'on');
+                }
+            }
+        }
+    }
+};
+
+/**
+ * monday = 0; tuesday = 1; ...
+ * */
+
+const mapDay = function (day) {
+    if (day === 'monday') return 0;
+    if (day === 'tuesday') return 1;
+    if (day === 'wednesday') return 2;
+    if (day === 'thursday') return 3;
+    if (day === 'friday') return 4;
+    if (day === 'saturday') return 5;
+    if (day === 'sunday') return 6;
 };
 
 const scheduleRepeatable = function (event) {
