93b05c0ad77a3d26d1612dea228fd99073be4bd8
gjcode <golla.programmazione@gmail.com>
2019-07-06 16:45:39 +0200

    aggiornati scripts di lancio

--- broker/api/routes/device-info.js
+++ broker/api/routes/device-info.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const fs = require('fs');
 
 router.get('/ip', (req, res, next) => {
-    return res.status(200).send("192.168.1.5");
+    return res.status(200).send('192.168.1.5');
 });
 
 module.exports = router;
