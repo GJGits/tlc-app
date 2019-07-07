fcdeef07fabb821837a309d1356af2d32be192a1
gjcode <golla.programmazione@gmail.com>
2019-07-07 16:29:14 +0200

    ip restituito in maniera dinamica

--- conf-devices/routes/index.js
+++ conf-devices/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const os = require('os');
 const ifaces = os.networkInterfaces();
+const fs = require('fs');
 let ip;
 
 /* GET home page. */
@@ -11,15 +12,20 @@ router.get('/', function (req, res, next) {
         wlan0.forEach(iface => {
             if (iface.family === "IPv4") {
                 ip = iface.address;
+                fs.writeFileSync(__dirname + '/ip.json', JSON.stringify({ip: ip}));
                 return res.send("address setted!");
             }
         });
     }
-    res.status(500).send("interface's IPv4 not found")
+    res.send(JSON.stringify(ifaces));
 });
 
 router.get('/ip', (req, res, next) => {
-   return ip ? res.send(ip) : res.status(400).send("interface's IPv4 not found");
+    if (ip) {
+        return res.send(ip);
+    }
+    const ipFromJson = fs.readFileSync(__dirname + '/ip.json');
+    return ipFromJson.ip ? res.send(ipFromJson.ip) : res.status(400).send("interface's IPv4 not found");
 });
 
 module.exports = router;
