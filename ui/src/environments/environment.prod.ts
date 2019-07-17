bc7a69caa42e76518b0d3d2d7da1ffdc6a308c51
gjcode <golla.programmazione@gmail.com>
2019-07-17 09:17:37 +0200

    ip new

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://raspberrypi:3000/',
+  baseUrl: 'http://192.168.43.66:3000/',
   groupId: 'PL19-16',
   awsURL: 'http://ec2-34-220-162-82.us-west-2.compute.amazonaws.com:5002'
 };
